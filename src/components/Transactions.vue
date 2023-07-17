ad236781b9c208797d978fd40e8e866d9888ca57
kipngeno-isaac <kipngeno.isaac@students.kisiiuniversity.ac.ke>
2023-07-17 21:55:11 +0300

    Add api data to page

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -1,49 +1,49 @@
 <template>
   <div class="card">
-    <h1> Transactions</h1>
-  
+    <h1>Transactions</h1>
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Account Number</th>
-      <th scope="col">Description</th>
-      <th scope="col">Transaction Type</th>
-      <th scope="col">Amount</th>
-      <th scope="col">Balance</th>
-
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark Otto</td>
-      <td>01239948484</td>
-      <td>Savings deposit</td>
-      <td>Deposit</td>
-      <td>120</td>
-      <td>120</td>
-    </tr>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark Otto</td>
-      <td>01239948484</td>
-      <td>Savings deposit</td>
-      <td>Deposit</td>
-      <td>120</td>
-      <td>120</td>
-    </tr>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark Otto</td>
-      <td>01239948484</td>
-      <td>Savings deposit</td>
-      <td>Deposit</td>
-      <td>120</td>
-      <td>120</td>
-    </tr>
-  </tbody>
-</table>
-</div>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Description</th>
+          <th scope="col">Transaction Type</th>
+          <th scope="col">Amount</th>
+          <th scope="col">Balance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="transaction in transactions" :key="transaction.id">
+          <td>{{ transaction.id }}</td>
+          <td>{{ transaction.description }}</td>
+          <td>{{ transaction.transaction_type }}</td>
+          <td>{{ transaction.amount }}</td>
+          <td>{{ transaction.balance }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      transactions: [],
+    };
+  },
+  mounted() {
+    this.fetchTransactions();
+  },
+  methods: {
+    async fetchTransactions() {
+      try {
+        const response = await fetch(
+          "http://127.0.0.1:8000/api/transactions/list"
+        ).then(response => response.json());
+        this.transactions = response;
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    },
+  },
+};
+</script>
